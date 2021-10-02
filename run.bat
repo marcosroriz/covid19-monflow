22217802174c9f2c2a2e3f0d904765bc3f7e5aa9
Marcos Roriz <marcosrorizinf@gmail.com>
2021-10-02 18:17:20 -0300

    Adicioneis mais modelos no script

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
-set list=centernet_hg104_1024x1024_coco17_tpu-32 efficientdet_d4_coco17_tpu-32 
+set list=centernet_hg104_1024x1024_coco17_tpu-32 efficientdet_d4_coco17_tpu-32 ssd_resnet50_v1_fpn_1024x1024_coco17_tpu-8 efficientdet_d7_coco17_tpu-32 ssd_resnet101_v1_fpn_1024x1024_coco17_tpu-8 ssd_resnet152_v1_fpn_1024x1024_coco17_tpu-8 faster_rcnn_resnet50_v1_1024x1024_coco17_tpu-8 faster_rcnn_resnet152_v1_1024x1024_coco17_tpu-8 faster_rcnn_inception_resnet_v2_1024x1024_coco17_tpu-8 faster_rcnn_resnet101_v1_1024x1024_coco17_tpu-8 
 (for %%a in (%list%) do ( 
    python main_process_raw.py --inputdir=C:/Imagens/T/ --model=%%a 
    python main_process_tiling.py --inputdir=C:/Imagens/T/ --model=%%a 
